6bf3f82184306123aee8c2eb92c3bf226968aef3
vipinrwt95 <vipinrwt9@gmail.com>
2022-12-17 17:09:55 +0530

    get profile details

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,7 +1,7 @@
 import { Container, Form } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import classes from '../components/Signupform.module.css';
-import React,{useRef,useContext} from "react";
+import React,{useRef,useContext,useState} from "react";
 import TokenContext from "../store/TokenContext";
 
 const Profile=()=>
@@ -9,19 +9,47 @@ const Profile=()=>
     const fullnameref=useRef();
     const profilephotoref=useRef();
     const authctx=useContext(TokenContext)
+    const[displayname,setname]=useState();
+    const[photoUrl,setUrl]=useState();
+
+    if(authctx.isLoggedIn)
+ {
+        fetch("https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCu84ggw5QyT2dJUlCmQvGfL2teJjDB1DE",
+      {
+        method:'POST',
+        body:JSON.stringify({
+          idToken:authctx.tokenid,  
+       }
+       )
+    }
+    ).then(res=>{
+        if(res.ok)
+        {
+          return res.json().then(data=>{
+             setname(data.displayName);
+             setUrl(data.photoUrl);
+             
+          })
+        }
+        else{
+          return res.json().then(data=>{
+           alert(data.error.message)
+          });
+        }})
+  } 
+
 
   const detailsformHandler=(event)=>
   {  event.preventDefault();
     const enteredname=fullnameref.current.value;
     const enteredurl=profilephotoref.current.value;
-    console.log(enteredurl)
-    console.log(authctx);
-    fetch("https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCu84ggw5QyT2dJUlCmQvGfL2teJjDB1DE",
+    
+fetch("https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCu84ggw5QyT2dJUlCmQvGfL2teJjDB1DE",
       {
         method:'POST',
         body:JSON.stringify({
           idToken:authctx.tokenid,  
-          displayname:enteredname,
+          displayName:enteredname,
           photoUrl:enteredurl,
           returnSecureToken:true
         }),
@@ -55,9 +83,9 @@ return(
     <Form onSubmit={detailsformHandler}>
      <div className={classes.auth}> 
     <label>Full Name</label>
-    <input type="text" required ref={fullnameref}/></div>  
+    <input type="text" required defaultValue={displayname} ref={fullnameref}/></div>  
     <div className={classes.auth}><label>Profile photo URL</label>
-     <input type="text" required ref={profilephotoref} /></div>
+     <input type="text" required defaultValue={photoUrl} ref={profilephotoref} /></div>
      <div className={classes.actions}>
         <button>UPDATE</button>
      </div>
